6a824f6dc25f248ce5a8de57350fffec4e3f649e
Ananth <mvananthkumar@gmail.com>
2022-05-31 11:32:41 +0530

    Commit

---
+++ 15-Exception-Handling/Built-in-Exception/Unchecked-Exception/UncheckedException.java
@@ -0,0 +1,19 @@
+import java.io.*;
+class UnCheckedException{
+	
+	public static void main(String []args) throws IOException{
+		
+		// Reading file from path in local directory
+		FileReader file = new FileReader("C:\\Users\\Unbeknownstguy\\Documents\\GitHub\\Java\\15-Exception-Handling\\Built-in-Exception\\Unchecked-Exception\\a.txt");
+		
+		// Creating object as one of ways of taking input
+		BufferedReader fileInput = new BufferedReader(file);
+		
+		for(int counter =0; counter<3; counter++)
+			System.out.println(fileInput.readLine());
+		
+		// Closing file connections
+		// using close() method
+		fileInput.close();
+	}
+}
